f0ac9f31c4cf4226e108c1d64edbd41754e08d83
luvrom2010 <sbuitrago142@gmail.com>
2025-08-27 20:41:42 -0500

    Update personaje.cpp
    en este primer commit se demuestra la creación de variables y clases de movimieno y el inicio de un sistema economico inicial para el juego

--- src/cpp/personaje.cpp
+++ src/cpp/personaje.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+class personaje {
+private:
+    int movimiento;
+    int dinero;
+
+public:
+    personaje(int m, int v) {
+        movimiento = m;
+        dinero = 1500;
+    }
+
+    void gasto(int cantidad) {
+        if (dinero >= cantidad) {
+            dinero -= cantidad;
+        }
+        else {
+            cout << "SIN SALDO" << endl;
+        }
+    }
+};
+
+int main() {
+    personaje heroe(5, 100);
+
+    return 0;
+}
